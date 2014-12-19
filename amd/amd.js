dbe08b8cd1db349bb03cd2eeddadbabb51511e86
Vladislav Churakov <pioneer32@mail.ru>
2014-12-19 19:26:53 +0200

    Add: Syntax easing... Some stable fixes

--- amd/amd.js
+++ amd/amd.js
@@ -1,5 +1,8 @@
 "use strict";
-
+/**
+ * @author Vladislav Churakov <pioneer32@mail.ru>
+ * @link https://github.com/pioneer32/js-collection/tree/master/amd
+ */
 ( function ( window ) {
 	var
 		options = {
@@ -9,11 +12,23 @@
 			modules : {}
 		},
 
+		working = false,
+
 		scrRegexp = new RegExp( 'scripts/(.+?)/([^/]+?)]\\.js$', 'i' ),
 		definedCnt = 0,
-		factories = {},
+		creators = {},
 		scripts = {},
 		dependencies = {},
+
+		_noop = function () {},
+
+		_isFunc = function ( subj ) {
+			return subj && Object.prototype.toString.call( subj ) === '[object Function]';
+		},
+
+		_isArray = function ( subj ) {
+			return subj && Object.prototype.toString.call( subj ) === '[object Array]';
+		},
 
 		_getScriptPrefix = function( n ) {
 			// Idea was found at http://www.eriwen.com/javascript/js-stack-trace/
@@ -23,37 +38,29 @@
 			return m ? m[ 1 ] + '/' : '';
 		},
 		_getDeps = function ( deps ) {
-			var r = [], il = deps.length, i = 0, m;
-			for ( ; m = options.modules[ deps[ i ] ], r[ i ] = m, i < il; i++ ) if ( !m ) throw new TypeError( 'Module ' + deps[ i ] + ' not available' );
+			var r = [], i = 0, il, m;
+			if ( _isArray( deps ) ) for ( il = deps.length; m = options.modules[ deps[ i ] ], r[ i ] = m, i < il; i++ ) if ( !m ) throw new TypeError( 'Module ' + deps[ i ] + ' not available' );
 			return r;
 		},
 		_dependencyRiched = function ( name ) {
 			var a, d = dependencies[ name ], i;
 			for ( a in dependencies ) {
 				if ( dependencies.hasOwnProperty( a ) ) {
-					if ( ( i = dependencies[ a ].indexOf( name ) ) !== -1 ) dependencies[ a ].splice( i, 1 );
-					if ( !dependencies[ a ].length ) {
-						options.modules[ a ] = factories[ a ]();
-						delete dependencies[ a ];
-						setTimeout( ( function ( a ) {
-							return function () {
-								_dependencyRiched( a );
-							}
-						} ) ( a ), 0 );
+					if ( ( i = dependencies[ a ].indexOf( name ) ) !== -1 ) {
+						dependencies[ a ].splice( i, 1 );
+						if ( !dependencies[ a ].length ) creators[ a ]();
 					}
 				}
 			}
 		},
 		_scriptLoaded = function ( res, name ) {
 			if ( !res ) throw new TypeError( 'Module ' + name + ' not loaded' );
-			if ( !scripts[ name ] || scripts[ name ] === true ) return;
-			clearTimeout( scripts[ name ] );
+			if ( scripts[ name ] === true ) return;
+			if ( scripts[ name ] ) clearTimeout( scripts[ name ] );
 			scripts[ name ] = true;
 		},
 		_loadScript = function ( name, url ) {
 			var prefix = name.split( '/' ), s;
-			if ( options.modules[ name ] ) throw new TypeError( 'Script with module ' + name + ' already loaded' );
-			if ( scripts[ name ] ) return;
 			prefix.pop();
 			s = document.createElement( "script" );
 			if ( prefix.length ) s.__prefix = prefix.join( '/' ) + '/';
@@ -62,8 +69,10 @@
 			s.src = url || ( options.urlPrefix + name + '.js' + ( options.cacheProtection ? '?_=' + ( new Date ).getTime() : '' ) );
 			scripts[ name ] = setTimeout( function () {
 				// TODO Timeout (... Trying to re-request is a good idea
-				_scriptLoaded( false, name );
-				s.onload = s.onerror = null;
+				/*
+				 _scriptLoaded( false, name );
+				 s.onload = s.onerror = null;
+				 */
 			}, 45000 ); // TODO Move to some 'constant' in head of file
 			s.onerror = function () {
 				// TODO Throw error, module can't be loaded
@@ -71,40 +80,61 @@
 				s.onload = s.onerror = null;
 			};
 			( document.getElementsByTagName( "body" )[ 0 ] || document.getElementsByTagName( "head" )[ 0 ] ).appendChild( s );
-		};
-
-	window.define = function ( name, deps, factory, skipSat ) {
-		var
-			creator = function () {
-				var t = { _moduleName : name, DEBUG : options.DEBUG[ name ] };
-				return factory ? ( factory.apply( t, deps ? _getDeps( deps ) : [] ) || t ) : true;
-			},
-			dep = [], i;
-
-		if ( !name ) throw new TypeError( 'Empty module name' );
-		name = _getScriptPrefix( name ) + name;
-		skipSat || _scriptLoaded( true, name );
-		if ( options.modules[ name ] ) throw new TypeError( 'Module ' + name + ' already defined' );
-		if ( deps && deps.length ) {
-			for ( i = deps.length; i--; ) {
-				if ( !options.modules[ deps[ i ] ] ) {
-					dep[ dep.length ] = deps[ i ];
-					_loadScript( deps[ i ] );
+		},
+		_registerModule = function ( name, module ) {
+			options.modules[ name ] = module || true;
+			delete creators[ name ];
+			delete dependencies[ name ];
+			setTimeout( ( function ( a ) {
+				return function () {
+					_dependencyRiched( a );
+				}
+			} ) ( name ), 0 );
+		},
+		_initModule = function ( name, deps, creator ) {
+			var dep = [], i;
+			if ( _isArray( deps ) ) {
+				for ( i = deps.length; i--; ) {
+					if ( !options.modules[ deps[ i ] ] ) {
+						dep[ dep.length ] = deps[ i ];
+						if ( scripts[ deps[ i ] ] ) continue;
+						_loadScript( deps[ i ] );
+					}
 				}
 			}
-		}
-		if ( !dep.length ) {
-			options.modules[ name ] = creator();
-			_dependencyRiched( name );
+			if ( !dep.length ) {
+				creator();
+			} else {
+				// TODO Add cross-dependencies check
+				creators[ name ] = creator;
+				dependencies[ name ] = dep;
+			}
+		};
+
+	window.define = function ( mod, deps, factory ) { // or just define( mod ) or define( mod, factory )
+		working = true;
+		if ( typeof mod !== "string" && mod.__moduleName  ) {
+			_registerModule( mod.__moduleName, mod );
 		} else {
-			// TODO Add cross-dependencies check
-			factories[ name ] = creator;
-			dependencies[ name ] = dep;
+			if ( !mod ) throw new TypeError( 'Empty module name' );
+			mod = _getScriptPrefix( mod ) + mod;
+			_scriptLoaded( true, mod );
+			if ( options.modules[ mod ] ) throw new TypeError( 'Module ' + mod + ' already defined' );
+			if ( !deps && !factory ) {
+				_registerModule( mod, true );
+			} else {
+				if ( _isFunc( deps ) ) factory = deps;
+				_initModule( mod, deps, function () {
+					var t = { __moduleName : mod, DEBUG : options.DEBUG[ mod ] };
+					_registerModule( mod, factory ? ( factory.apply( t, _getDeps( deps ) ) || t ) : true );
+				} );
+			}
 		}
 	};
 
 	window.define.options = function ( opts ) {
 		var a;
+		if ( working ) throw new TypeError( 'Can\'t apply new options (... ' );
 		for ( a in opts ) if ( opts.hasOwnProperty( a ) ) options[ a ] = opts[ a ];
 		if ( options.urlPrefix !== '' ) options.urlPrefix += '/';
 		// TODO Possibly escaping of options.urlPrefix needed
@@ -112,19 +142,29 @@
 		options.DEBUG || ( options.DEBUG = {} );
 	};
 
-	window.require = function ( modules, callBack ) {
-		if ( modules && modules.length ) {
-			window.define( '__' + ( definedCnt++ ), modules, callBack, true );
-		} else {
-			callBack();
-		}
+	window.expect = function ( mod, deps, factory ) { // or just expect( mod, factory )
+		working = true;
+		if ( typeof mod !== "string" ) throw new TypeError( 'For direct module define you should use define(...)' );
+		if ( !mod ) throw new TypeError( 'Empty module name' );
+		mod = _getScriptPrefix( mod ) + mod;
+		_scriptLoaded( true, mod );
+		if ( options.modules[ mod ] ) throw new TypeError( 'Module ' + mod + ' already defined' );
+		if ( _isFunc( deps ) ) factory = deps;
+		if ( !_isFunc( factory ) ) throw new TypeError( 'Factory must be a Function' );
+		_initModule( mod, deps, function () {
+			factory.apply( { __moduleName : mod, DEBUG : options.DEBUG[ mod ] }, _getDeps( deps ) );
+		} );
 	};
 
-	/*
-	 window.require = function ( name ) {
-	 if ( !options.modules[ name ] ) throw new TypeError( 'Module ' + name + ' not defined' );
-	 return options.modules[ name ];
-	 }
-	 */
+	window.require = function ( modules, callBack ) { // or just require( modules )
+		working = true;
+		if ( _isArray( modules ) && modules.length ) {
+			_initModule( '__' + ( definedCnt++ ), modules, _isFunc( callBack ) ? function () {
+				callBack.apply( {}, _getDeps( modules ) );
+			} : _noop );
+		} else {
+			throw new TypeError( 'Required list of modules is empty' );
+		}
+	};
 
 } )( window );
